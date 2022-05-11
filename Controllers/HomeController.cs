5b09ac8110a5c02c907315957d7f9d368f32cc2e
Roberto Silva <fsilva@compugraf.com.br>
2022-05-11 12:46:13 -0300

    add Documentation Swagger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiAuth.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("user")]
     public class HomeController : ControllerBase
     {
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using ApiAuth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,51 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    {
+        Version = "v1",
+        Title = "Access e Refresh Token",
+        Description = "Autenticação via Token",
+        //TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Repositório",
+            Url = new Uri("https://github.com/beto-frs/Access_E_Refresh_Token")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Roberto Silva",
+            Url = new Uri("https://www.linkedin.com/in/fcorobertodasilva/")
+        }
+    });
+
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer'[space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 
 var key = Encoding.ASCII.GetBytes(Settings.Secret);
 
